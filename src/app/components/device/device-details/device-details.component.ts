94b5406df2cb4c1c40fe44f2194610806b4ba410
Parillo Mattia, P114 <mattia.parillo@post.ch~>
2024-07-29 17:21:51 +0200

    drag and drop functional

---
+++ src/app/components/device/device-details/device-details.component.ts
@@ -0,0 +1,38 @@
+import {Component, OnInit} from '@angular/core';
+import {DeviceService} from '../../../services/device.service';
+import {ActivatedRoute} from '@angular/router';
+import {ErrorHandlerService} from '../../../services/error-handler.service';
+import {Device} from '../../../models/Device';
+
+@Component({
+  selector: 'app-device-details',
+  templateUrl: './device-details.component.html',
+  styleUrl: './device-details.component.scss'
+})
+export class DeviceDetailsComponent implements OnInit {
+  device?: Device;
+
+  constructor(private deviceService: DeviceService,
+              private route: ActivatedRoute,
+              private errorHandler: ErrorHandlerService) {
+  }
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      const uuId = params['id'];
+      if (uuId !== null && uuId !== undefined) {
+        this.getDeviceDetails(uuId);
+      }
+    });
+  }
+
+  private getDeviceDetails(uuId: number) {
+    this.deviceService.getDevice(uuId).subscribe(res => {
+      if (this.errorHandler.hasError(res)) {
+        this.errorHandler.setErrorMessage(res.errorMessage!);
+      } else {
+        this.device = res.object;
+      }
+    });
+  }
+}
